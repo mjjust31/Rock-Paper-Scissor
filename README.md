b98ac6761a755e086a902bd17cea081786a78c7f
justmann <mj.justmann@gmail.com>
2024-01-23 21:25:03 -0600

    basic game

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Rock-Paper-Scissor
+
+Build a game using only JavaScript.
+
+Instructions:
+
+The completed application should meet the following criteria:
+
+As a user, I want to play Rock, Paper, Scissors against an automated opponent.
+
+As a user, I can enter R, P, or S to signify my choice of rock, paper, or scissors.
+
+As a user, I expect the computer to choose R, P, or S in return.
+
+As a user, I want the option to play again whether I win or lose.
+
+As a user, I want to see my total wins, ties, and losses after each round.
+
+Specifications
+Must use the alert(), confirm(), and prompt() methods to collect user input and display information to the user.
+
+The computer's selection must be random to ensure a fair game.
--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 let wins = 0;
 let loss = 0;
 let ties = 0;
-const computerPick = ["r", "p", "s"];
+const compChoices = ["r", "p", "s"];
 let userPick = "";
+let compPick = "";
 
 const playGame = () => {
   let userPick = prompt(
     "Please enter 'R' for Rock, 'P' for Paper, 'S' for scissor"
   );
+  console.log(userPick);
+  let compPick = getComputerPick();
+  console.log(compPick);
 
-  if (computerPick === userPick) {
+  if (compPick === userPick) {
     alert("it's a tie!");
     ties++;
-  } else if (computerPick === 'r' && userPick ==='s' || computerPick==='p' && userPick ==='s' || computerPick === 's' && userPick==='p')
+  } else if (
+    (compPick === "r" && userPick === "s") ||
+    (compPick === "p" && userPick === "r") ||
+    (compPick === "s" && userPick === "p")
+  ) {
+    alert("You lost!");
+    loss++;
+  } else {
+    alert("You win!");
+    wins++;
+  }
 };
 
 const getComputerPick = () => {
-  let random = Math.floor(Math.random() * computerPick.length);
-  let compPick = computerPick[random];
+  let random = Math.floor(Math.random() * compChoices.length);
+  let compPick = compChoices[random];
 
   return compPick;
 };
